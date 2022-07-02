43a460816b225c0b772e0001bfc425863d00eb7a
sviosdi <sviosdi@yandex.ru>
2022-07-03 00:46:26 +0200

    =update

--- P6/scripts/pages/fetchData.js
+++ P6/scripts/pages/fetchData.js
@@ -1,6 +1,6 @@
 // retourne le tableau des photographes. [{ phgr1 }, { phgr2 } ...]
 async function fetchPhotographers() {
-        return await fetch("data/photographers.json")
+        return await fetch("https://raw.githubusercontent.com/sviosdi/sviosdi.github.io/main/P6/data/photographers.json")
             .then(res => res.json())         // res.json() = { photgraphers:[{},{} ...], media: [{},{} ...]}
             .then(res => res.photographers)  // res.photographers = [{ phgr1 }, { phgr2 } ...]
             .catch(err => console.log("erreur lors du chargement de la liste des photographes", err));
@@ -9,7 +9,7 @@ async function fetchPhotographers() {
  
 
 async function fetchData() {
-    return await fetch("data/photographers.json")
+    return await fetch("https://raw.githubusercontent.com/sviosdi/sviosdi.github.io/main/P6/data/photographers.json")
         .then(res => res.json())   // res.json() = { photgraphers:[{},{} ...], media: [{},{} ...]}
         .then(res => res)  
         .catch(err => console.log("erreur lors du chargement de la liste des photographes", err));
